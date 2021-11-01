2b6c0d413674002e10e253a1ef100095bc9f1eb1
Rupert <rupert@penbrothers.com>
2021-11-01 10:46:18 +0800

    change heroku link

--- src/config/env.js
+++ src/config/env.js
@@ -1,6 +1,6 @@
 export const STRIPE_TEST_PUBLISHABLE_KEY =
   "pk_test_51JhioJGjIYcUCXhqKQcWJ6xTFT0p7BU6cT5CnTBoaGuGndkrzYmbHmbd90XC18MDChXnYiqJgn5ErCuxHI4gdUMs00kq1eIkr4";
 // export const API_MAIN = "http://localhost:8080/api/v1";
-export const API_MAIN = "https://grab-a-tea-api.herokuapp.com";
+export const API_MAIN = "https://grab-a-tea-api.herokuapp.com/api/v1";
 // export const API_MAIN = "https://grab-a-tea-api.herokuapp.com/";
 export const BOBA_REWARDS_RATE = 0.05;
